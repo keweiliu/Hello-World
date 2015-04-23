adb24e41f26d9df66afde2aff1c362a5f21f29c8
keweiliu <keweiliu@tapatalk.com>
2015-04-23 15:35:45 +0800

    release xf10 v1.1.5

--- xenforo/mobiquo/config/config.php
+++ xenforo/mobiquo/config/config.php
@@ -5,7 +5,7 @@ defined('IN_MOBIQUO') or exit;
 class TTConfig{
     public static function get_config(){
         return array(
-            'version' => 'xf10_1.1.4',
+            'version' => 'xf10_1.1.5',
             'api_level' => 3,
 
             'mark_forum' => 1,
